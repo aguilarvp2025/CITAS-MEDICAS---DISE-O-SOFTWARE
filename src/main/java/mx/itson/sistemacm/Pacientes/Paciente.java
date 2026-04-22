51f5fe935a4a0fbab517293dd4bd8d46f6e93a5f
aguilarvp2025 <capricho3000@outlook.com>
2026-04-21 22:50:45 -0700

    estructura basica

---
+++ src/main/java/mx/itson/sistemacm/Pacientes/Paciente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.sistemacm.Pacientes;
+
+/**
+ *
+ * @author capri
+ */
+public class Paciente {
+    
+}
